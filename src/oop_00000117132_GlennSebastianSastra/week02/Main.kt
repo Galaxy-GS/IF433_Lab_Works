b450be4abd017fe2515ffdfef6297df25b49fec4
GalaxyGS <glenn.s.sastra@gmail.com>
2026-02-18 20:55:27 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000117132_GlennSebastianSastra/week02/Main.kt
@@ -0,0 +1,2 @@
+package oop_00000117132_GlennSebastianSastra.week02
+
--- src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
+++ src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
@@ -4,11 +4,4 @@ class Student(
     val nim: String,
     val name: String,
     var major: String
-) {
-
-    init {
-        if (nim.length != 8) {
-            println("WARNING: NIM harus 8 digit!")
-        }
-    }
-}
+)
