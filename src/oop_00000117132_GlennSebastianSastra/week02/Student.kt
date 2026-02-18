08f6d35fb8dd028e0275fbf96396eec2499addf9
GalaxyGS <glenn.s.sastra@gmail.com>
2026-02-18 20:52:53 +0700

    week02: add warning validation in init block

--- src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
+++ src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
@@ -4,4 +4,11 @@ class Student(
     val nim: String,
     val name: String,
     var major: String
-)
+) {
+
+    init {
+        if (nim.length != 8) {
+            println("WARNING: NIM harus 8 digit!")
+        }
+    }
+}
