cb1bafd022f6353e6cf7499a4b0bd8d69934f4fc
GalaxyGS <glenn.s.sastra@gmail.com>
2026-02-18 20:56:58 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
+++ src/oop_00000117132_GlennSebastianSastra/week02/Student.kt
@@ -3,9 +3,9 @@ package oop_00000117132_GlennSebastianSastra.week02
 class Student(
     val nim: String,
     val name: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
-
     init {
         if (nim.length != 8) {
             println("WARNING: NIM harus 8 digit!")
