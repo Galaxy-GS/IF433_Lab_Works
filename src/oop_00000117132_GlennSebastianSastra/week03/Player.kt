279e8ddb2164918aa5bc458b0de2e755cc5cf29f
GalaxyGS <glenn.s.sastra@gmail.com>
2026-02-20 11:50:39 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000117132_GlennSebastianSastra/week03/Player.kt
@@ -0,0 +1,20 @@
+package oop_00000117132_GlennSebastianSastra.week03
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXP (amount: Int) {
+        if (xp <= 0) return
+
+        val oldLevel = level
+        xp += amount
+        val newLevel = level
+
+        if (newLevel > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $newLevel ")
+        }
+    }
+}
