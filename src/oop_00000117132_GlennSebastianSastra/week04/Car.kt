ec46858c98724ff2969ba3d3960e486387f892f6
GalaxyGS <glenn.s.sastra@gmail.com>
2026-02-27 10:26:58 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000117132_GlennSebastianSastra/week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_00000117132_GlennSebastianSastra.week04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
