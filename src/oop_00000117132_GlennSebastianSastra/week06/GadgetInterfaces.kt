3592a73c758f7546d7e53297274a6a9a6b847caf
GalaxyGS <glenn.s.sastra@gmail.com>
2026-03-13 10:11:47 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000117132_GlennSebastianSastra/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000117132_GlennSebastianSastra.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
