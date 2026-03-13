9c62c1f2c35aee0090c47963211e9ac91bb2c7cd
GalaxyGS <glenn.s.sastra@gmail.com>
2026-03-13 10:41:23 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000117132_GlennSebastianSastra/week06/SmartSpeaker.kt
@@ -0,0 +1,15 @@
+package oop_00000117132_GlennSebastianSastra.week06
+
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("$name dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("$name dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
