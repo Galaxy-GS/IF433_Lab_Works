ba84540f95597f217fa0ee4cfc12adf2fd6d7660
GalaxyGS <glenn.s.sastra@gmail.com>
2026-03-13 10:14:36 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000117132_GlennSebastianSastra/week06/Smartphone.kt
+++ src/oop_00000117132_GlennSebastianSastra/week06/Smartphone.kt
@@ -1,4 +1,9 @@
 package oop_00000117132_GlennSebastianSastra.week06
 
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smarthphone berhasil booting.")
+    }
 }
