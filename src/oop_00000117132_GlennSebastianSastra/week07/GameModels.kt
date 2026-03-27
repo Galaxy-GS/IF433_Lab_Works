3fd57b6b6f71e4b2a699129beb104c2fc24c6f49
GalaxyGS <glenn.s.sastra@gmail.com>
2026-03-27 11:10:41 +0700

    week07: (task) create GameItem data class

--- src/oop_00000117132_GlennSebastianSastra/week07/GameModels.kt
+++ src/oop_00000117132_GlennSebastianSastra/week07/GameModels.kt
@@ -3,3 +3,5 @@ package oop_00000117132_GlennSebastianSastra.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(50), RARE(25), EPIC(15), LEGENDARY(5), SECRET(1)
 }
+
+data class Item(val name: String, val damage: Int, val rarity: ItemRarity)
