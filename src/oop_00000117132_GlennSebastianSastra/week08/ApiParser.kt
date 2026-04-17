208a5be8589ed99573a7fb2dcf5165b2b7f810b6
GalaxyGS <glenn.s.sastra@gmail.com>
2026-04-17 15:39:00 +0700

    week08: (task) create JavaPaymentService for payment interop

--- src/oop_00000117132_GlennSebastianSastra/week08/ApiParser.kt
+++ src/oop_00000117132_GlennSebastianSastra/week08/ApiParser.kt
@@ -22,6 +22,7 @@ class ApiParser {
                 Product.Clothing(id, name, size)
             }
             else -> null
+
         }
     }
 }
---
+++ src/oop_00000117132_GlennSebastianSastra/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000117132_GlennSebastianSastra.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productID) {
+        return "TRX-" + productID + "-SUCCESS";
+    }
+}
