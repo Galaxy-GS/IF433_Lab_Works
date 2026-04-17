0d59d280f12849e98a043978aedf930f5600e0f0
GalaxyGS <glenn.s.sastra@gmail.com>
2026-04-17 15:13:14 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000117132_GlennSebastianSastra/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000117132_GlennSebastianSastra.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
