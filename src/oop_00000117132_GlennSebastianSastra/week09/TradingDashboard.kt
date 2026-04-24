50baec86fa357f54a90bd8130d8667d8f3fc57a8
GalaxyGS <glenn.s.sastra@gmail.com>
2026-04-24 13:33:55 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000117132_GlennSebastianSastra/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000117132_GlennSebastianSastra.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 7.0, "OPEN"),
+        TradeLog("XRPUSDT", "LONG", 25, 20.0, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 5, -2.5, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 10, 0.0, "OPEN"),
+    )
+}
