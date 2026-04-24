2557268e0c3028fe53c761c4f17958761450c737
GalaxyGS <glenn.s.sastra@gmail.com>
2026-04-24 10:40:42 +0700

    week09: implement map for key-value storage

--- src/oop_00000117132_GlennSebastianSastra/week09/CollectionBasics.kt
+++ src/oop_00000117132_GlennSebastianSastra/week09/CollectionBasics.kt
@@ -18,4 +18,17 @@ fun main() {
     activateUsers.add("UserC")
     activateUsers.add("UserD")
     println("Active Users: $activateUsers")
+
+    println("\n=== TEST MAP ===")
+    val studentGrades = mapOf(
+        "Alice" to "A",
+        "Bob" to "B",
+        "Charlie" to "C",
+    )
+    println("Nilai Bob: ${studentGrades["Bob"]}")
+
+    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
+    inventory["Oranges"] = 20
+    inventory["Apples"] = 45
+    println("Inventory: $inventory")
 }
