480cb90eebb61b11ff61814887faa4cd148c0740
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-07 16:19:38 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
+++ src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
@@ -5,4 +5,8 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.25))
     coinRepo.add(Coin("ETH", 10.5))
     coinRepo.add(Coin("USDT", 5000.0))
+
+    val response = ApiResponse(
+        "200 OK", coinRepo.getAll()
+    )
 }
