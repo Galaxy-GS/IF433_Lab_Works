d6e0a35ebba667faf0786c9485de9c63cf27317d
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-07 16:18:03 +0700

    week10: (task) populate coin repository

--- src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
+++ src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000117132_GlennSebastianSastra.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 10.5))
+    coinRepo.add(Coin("USDT", 5000.0))
 }
