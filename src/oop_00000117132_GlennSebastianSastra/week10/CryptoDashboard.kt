5b65bdbff7a0d80775f5e2b55db2e9c3897b6bcd
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-07 16:25:08 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
+++ src/oop_00000117132_GlennSebastianSastra/week10/CryptoDashboard.kt
@@ -16,4 +16,13 @@ fun main() {
     response.data.forEach {
         println("Coin: ${it.name} | Balance: ${it.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 150000.0))
+    txRepo.add(Transaction("TX002", 275000.0))
+    println("\n=== TRANSACTIONS ===")
+
+    txRepo.getAll().forEach {
+        println("Transaction ${it.id} | Amount: ${it.amount}")
+    }
 }
