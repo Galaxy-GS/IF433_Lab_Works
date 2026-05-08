4c3c4f1be60ed8da32152503cf5b68c24286b560
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-08 10:24:46 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000117132_GlennSebastianSastra/week11/SmartHomeDashboard.kt
+++ src/oop_00000117132_GlennSebastianSastra/week11/SmartHomeDashboard.kt
@@ -2,4 +2,14 @@ package oop_00000117132_GlennSebastianSastra.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice(
+        "Phillips WiZ Living Room",
+        "Lighting"
+    ).apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
