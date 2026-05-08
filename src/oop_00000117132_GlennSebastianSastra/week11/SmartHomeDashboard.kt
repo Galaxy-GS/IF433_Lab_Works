5d5eeeb61a7bc76bdfe7a07c4d587862ae69a2e7
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-08 10:26:53 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000117132_GlennSebastianSastra/week11/SmartHomeDashboard.kt
+++ src/oop_00000117132_GlennSebastianSastra/week11/SmartHomeDashboard.kt
@@ -4,12 +4,21 @@ fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
     SmartDevice(
-        "Phillips WiZ Living Room",
-        "Lighting"
+        "Phillips WiZ Living Room", "Lighting"
     ).apply {
         isOnline = true
         powerLoad = 12
     }.also {
+        homeDevices.add(it)
+    }
+
+    SmartDevice(
+        "Ezviz Outdoor", "Camera"
+    ).apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
 }
