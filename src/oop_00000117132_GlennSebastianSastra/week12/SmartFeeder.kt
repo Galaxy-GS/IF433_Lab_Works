8392dfee0829734789a707c5087741a71abaf53b
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-15 10:27:10 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000117132_GlennSebastianSastra/week12/SmartFeeder.kt
+++ src/oop_00000117132_GlennSebastianSastra/week12/SmartFeeder.kt
@@ -17,4 +17,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(80, currentKibbleStock, false)
+    } catch (e: DispenserJamException) {
+        println("Error Dispenser: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Stock: ${e.message}")
+    } catch (e: Exception) {
+        println("General Error: ${e.message}")
+    }
 }
