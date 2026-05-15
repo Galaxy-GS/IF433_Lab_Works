823a9cd274b2a91b9c27c5bc24c0c2cfcee97187
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-15 10:07:08 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000117132_GlennSebastianSastra/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000117132_GlennSebastianSastra.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
