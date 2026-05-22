3bc6b38bbdc1a136e79d7e738f5c107fbf50a6df
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-22 15:45:11 +0700

    week13: (task) test full crypto trade logger pipeline

--- src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
+++ src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
@@ -49,4 +49,9 @@ fun main() {
 
     val loadedData = loadTrades("crypto_trades.csv")
     val totalPnl = loadedData.sumOf { it.pnl }
+
+    loadedData.forEach {
+        println(it)
+    }
+    println("==== TOTAL PnL BERSIH: $totalPnl ====")
 }
