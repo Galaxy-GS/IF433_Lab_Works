b6ced89fcb41a8f05e789aaaf0325cc4cb9f3285
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-22 15:33:21 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
+++ src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
@@ -1,6 +1,7 @@
 package oop_00000117132_GlennSebastianSastra.week13
 
 import java.io.File
+import java.io.FileNotFoundException
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
@@ -24,5 +25,14 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach {
             writer.println(it.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path).readLines().mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        println("ERROR: File trade tidak ditemukan!")
+        emptyList()
     }
 }
