35391a97d0c31f09d3ebe3f4c706fbcf0fc42371
GalaxyGS <glenn.s.sastra@gmail.com>
2026-05-22 15:30:00 +0700

    week13: (task) implement saveTrades using use block and printWriter

--- src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
+++ src/oop_00000117132_GlennSebastianSastra/week13/CryptoTradeLogger.kt
@@ -1,5 +1,7 @@
 package oop_00000117132_GlennSebastianSastra.week13
 
+import java.io.File
+
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
 fun TradeRecord.toCsv(): String {
@@ -14,5 +16,13 @@ fun fromCsvTrade(line: String): TradeRecord? {
     } catch (e: Exception) {
         println("(Log) Data korup diabaikan: $line")
         null
+    }
+}
+
+fun saveTrades(trades: List<TradeRecord>, path: String) {
+    File(path).printWriter().use { writer ->
+        trades.forEach {
+            writer.println(it.toCsv())
+        }
     }
 }
